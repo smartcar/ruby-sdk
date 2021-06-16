7fb35ab5e1045a895cd8fe1d4aee68fb859d79c5
s-ashwinkumar <s.ashwinkumar2490@gmail.com>
2021-06-15 23:16:48 -0700

    feat: modify vehicle instance methods(without subscribe, unsubscribe)

--- lib/smartcar/utils.rb
+++ lib/smartcar/utils.rb
@@ -25,6 +25,18 @@ module Smartcar
       ENV[config_name]
     end
 
+    def build_response(body, meta)
+      JSON.parse(body.merge(meta: meta).to_json, object_class: OpenStruct)
+    end
+
+    def build_aliases(response, aliases)
+      (aliases || []).each do |original_name, alias_name|
+        response.send("#{alias_name}=".to_sym, response.send(original_name.to_sym))
+      end
+
+      response
+    end
+
     # Given the response from smartcar API, returns an error object if needed
     # @param response [Object] response Object with status and body
     #
@@ -42,6 +54,47 @@ module Smartcar
     def handle_errors(response)
       error = get_error(response)
       raise error if error
+    end
+
+    def process_batch_response(response)
+      response_object = OpenStruct.new
+      response['responses'].each do |item|
+        attribute_name = item['path'][1..-1]
+        aliases = Vehicle::METHODS[attribute_name.to_sym][:aliases]
+        response_body = build_aliases(build_response(item['body'], item['headers']), aliases)
+        response_object.define_singleton_method attribute_name do
+          if response_body.error || response_body.statusCode
+            raise Smartcar::ExternalServiceError,
+                  "API error - #{response_body}"
+          end
+
+          response_body
+        end
+      end
+      response_object
+    end
+
+    def get_batch_request_body(paths)
+      attributes = validated_attributes(paths)
+      requests = attributes.each_with_object([]) do |item, all_requests|
+        all_requests << { path: get_path(item) }
+      end
+      { requests: requests }
+    end
+
+    def get_path(attribute)
+      path = Vehicle::METHODS[attribute][:path].call(id)
+      path.split("/vehicles/#{id}").last
+    end
+
+    def validated_attributes(paths)
+      attributes = paths.map { |path| path[1..-1].to_sym }
+      unsupported_attributes = (attributes - Vehicle::BATCH_SUPPORTED_METHODS) || []
+      unless unsupported_attributes.empty?
+        message = "Unsupported attribute(s) requested in batch  - #{unsupported_attributes.join(',')}"
+        raise Smartcar::Base::InvalidParameterValue.new, message
+      end
+      attributes
     end
   end
 end
