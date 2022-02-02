52dc1ca935c401aa28296abbbc3a1491d8ce4c3f
naomiperez <naomisperez99@gmail.com>
2022-02-01 17:45:25 -0800

    fix: set request() path to lowercase

--- lib/smartcar/vehicle.rb
+++ lib/smartcar/vehicle.rb
@@ -246,8 +246,8 @@ module Smartcar
     # @return [OpenStruct] An object with a "body" attribute that contains the
     #   response body and a "meta" attribute with the relevant items from response headers.
     def request(method, path, body = {}, headers = {})
-      path = "/vehicles/#{id}/#{path}"
-      raw_response, headers = send(method, path, body, headers)
+      path = "/vehicles/#{id}/#{path.downcase}"
+      raw_response, headers = send(method, path.downcase, body, headers)
       meta = build_meta(headers)
       json_to_ostruct({ body: raw_response, meta: meta })
     end
