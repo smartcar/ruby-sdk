da95be35968722cfecd1b1672327bf06bfd3d66a
Haig (Hike) Hovsepian <hhovsepi@gmail.com>
2024-04-30 09:19:00 -0500

    fix indentation issues

--- spec/smartcar/e2e/smartcar_error_spec.rb
+++ spec/smartcar/e2e/smartcar_error_spec.rb
@@ -63,40 +63,39 @@ Please prompt the user to re-authenticate using Smartcar Connect."
   end
 end
 
-  #  Note: we skip the following 2 tests because the account associated with them
-  #  began throwing Smartcar Authentication errors. At this time, we have not diagnosed
-  #  exactly why this is happening, so we skip for now to move projects forward.
+#  Note: we skip the following 2 tests because the account associated with them
+#  began throwing Smartcar Authentication errors. At this time, we have not diagnosed
+#  exactly why this is happening, so we skip for now to move projects forward.
 
-  # context 'with resolution string' do
-  #   it 'should convert the resolution string to object and fill the value in type' do
-  #     vehicle = get_vehicle_object('VEHICLE_STATE.UNKNOWN@smartcar.com')
-  #     expected_description = 'The vehicle was unable to perform your request due to an unknown issue.'
-  #     expect { vehicle.odometer }.to(raise_error do |error|
-  #       expect(error.status_code).to eq(409)
-  #       expect(error.type).to eq('VEHICLE_STATE')
-  #       expect(error.code).to eq('UNKNOWN')
-  #       expect(error.description).to eq(expected_description)
-  #       expect(error.doc_url).to eq('https://smartcar.com/docs/errors/api-errors/vehicle-state-errors#unknown')
-  #       expect(error.resolution.type).to eq('RETRY_LATER')
-  #       expect(error.message).to eq("VEHICLE_STATE:UNKNOWN - #{expected_description}")
-  #     end)
-  #   end
-  # end
+# context 'with resolution string' do
+#   it 'should convert the resolution string to object and fill the value in type' do
+#     vehicle = get_vehicle_object('VEHICLE_STATE.UNKNOWN@smartcar.com')
+#     expected_description = 'The vehicle was unable to perform your request due to an unknown issue.'
+#     expect { vehicle.odometer }.to(raise_error do |error|
+#       expect(error.status_code).to eq(409)
+#       expect(error.type).to eq('VEHICLE_STATE')
+#       expect(error.code).to eq('UNKNOWN')
+#       expect(error.description).to eq(expected_description)
+#       expect(error.doc_url).to eq('https://smartcar.com/docs/errors/api-errors/vehicle-state-errors#unknown')
+#       expect(error.resolution.type).to eq('RETRY_LATER')
+#       expect(error.message).to eq("VEHICLE_STATE:UNKNOWN - #{expected_description}")
+#     end)
+#   end
+# end
 
-  # context 'with null resolution' do
-  #   it 'should have empty resolution' do
-  #     vehicle = get_vehicle_object('CONNECTED_SERVICES_ACCOUNT.VEHICLE_MISSING@smartcar.com')
-  #     expected_description = "This vehicle is no longer associated with the user's connected services account. "\
-  #     "Please prompt the user to re-add the vehicle to their account."
-  #     expect { vehicle.odometer }.to(raise_error do |error|
-  #       expect(error.status_code).to eq(400)
-  #       expect(error.type).to eq('CONNECTED_SERVICES_ACCOUNT')
-  #       expect(error.code).to eq('VEHICLE_MISSING')
-  #       expect(error.description).to eq(expected_description)
-  #       expect(error.doc_url).to eq('https://smartcar.com/docs/errors/api-errors/connected-services-account-errors#vehicle-missing')
-  #       expect(error.resolution.type).to be_nil
-  #       expect(error.message).to eq("CONNECTED_SERVICES_ACCOUNT:VEHICLE_MISSING - #{expected_description}")
-  #     end)
-  #   end
-  # end
-
+# context 'with null resolution' do
+#   it 'should have empty resolution' do
+#     vehicle = get_vehicle_object('CONNECTED_SERVICES_ACCOUNT.VEHICLE_MISSING@smartcar.com')
+#     expected_description = "This vehicle is no longer associated with the user's connected services account. "\
+#     "Please prompt the user to re-add the vehicle to their account."
+#     expect { vehicle.odometer }.to(raise_error do |error|
+#       expect(error.status_code).to eq(400)
+#       expect(error.type).to eq('CONNECTED_SERVICES_ACCOUNT')
+#       expect(error.code).to eq('VEHICLE_MISSING')
+#       expect(error.description).to eq(expected_description)
+#       expect(error.doc_url).to eq('https://smartcar.com/docs/errors/api-errors/connected-services-account-errors#vehicle-missing')
+#       expect(error.resolution.type).to be_nil
+#       expect(error.message).to eq("CONNECTED_SERVICES_ACCOUNT:VEHICLE_MISSING - #{expected_description}")
+#     end)
+#   end
+# end
